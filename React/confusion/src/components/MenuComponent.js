fdfdc42fb3efecc5a827bffe7318e8455a93dddd
Yoong Tran <yoong.thuydung@shopee.com>
2020-11-17 00:30:19 +0800

    Presentational and Container Components

--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -24,8 +24,8 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div  className="col-12 col-md-5 m-1" key={dish.id}>
-                <Card
-                  onClick={() => this.onDishSelect(dish)}>
+                <Card key={dish.id}
+                        onClick={() => this.props.onClick(dish.id)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -40,12 +40,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                {/* <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div> */}
-                <DishDetail dish={this.state.selectedDish}/>
             </div>
         );
     }
