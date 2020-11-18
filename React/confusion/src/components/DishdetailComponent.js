08e3bfa2c1eb2644d36f8641dd375308839b854c
Yoong Tran <yoong.thuydung@shopee.com>
2020-11-18 12:53:44 +0800

    Single Page Applications Part 2

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
     
     // componentDidMount() {
     //     console.log('DishDetail component componentDidMount invoked')
@@ -74,11 +75,28 @@ const DishDetail = (props) => {
     // console.log(this.props);
     if (props.dish!=null) {
         return (
+            <div className="container">
             <div className="row">
-                <RenderDish dish={props.dish} />
-                <RenderComments comments={props.dish.comments} />
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
             </div>
-        )
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+            </div>
+        );
     }
     else {
         return (
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -36,7 +36,14 @@ class Main extends Component {
               leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
-    }
+    };
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     
     return (
       <div>
@@ -45,6 +52,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route exact path='/contactus' component={Contact} />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Redirect to="/home" />
         </Switch>
         <Footer />
